c56895f82f303631e8bae46fa5fc095be74bb57f
Nikksys <n.b.nixsys@gmail.com>
2022-03-21 16:40:40 -0500

    delivery mailer method to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {host: "https://nixsys-portfolio.herokuapp.com/"}
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
